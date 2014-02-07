0933d20e07071382fd7cced4367402bd3c3a9f51
bratwursted <joe@thinxmedia.com>
2014-02-06 19:13:35 -0500

    Initial Commit

---
+++ SportsNewsDemo/TSNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TSNAppDelegate.h
+//  SportsNewsDemo
+//
+//  Created by Joe on 2/6/14.
+//  Copyright (c) 2014 Thinx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SportsNewsDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SportsNewsDemo
+//
+//  Created by Joe on 2/6/14.
+//  Copyright (c) 2014 Thinx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TSNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TSNAppDelegate class]));
+    }
+}
